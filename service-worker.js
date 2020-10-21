8bb7e30205b926369144b9b2ec7117b9ff3698b2
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-10-21 09:35:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.03a8096817e84823445e9c0b1b35bf72.js"
+  "./precache-manifest.4722b5146d21f0b915be87242a636903.js"
 );
 
 self.addEventListener('message', (event) => {
